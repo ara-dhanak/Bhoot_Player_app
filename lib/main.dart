32d30bbe59f1d67f96a0bab165beb8195d80e004
sahnnu <sahnnu@yahoo.com>
2021-02-21 21:32:10 +0530

    Added Android webview so that we can utilize the bhootnath webpage in our page 2 directly

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'LoginPage.dart';
 
 void main() =>
     runApp(new MaterialApp(
-      // home: ScaffoldExample())
-      //  home: BizzCard())
-      //home: bhoot_bottom_nav()),
         debugShowCheckedModeBanner: false,
         home: LoginPage())
 
